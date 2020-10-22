0cd8e4d150b30310bbb63f1a91663e6729cc22ea
Lucas Julián Güerci <lucas.guerci@gmail.com>
2020-10-21 22:12:18 -0300

    Added crosshair

---
+++ Assets/Scripts/Player/InteractWith.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractWith : MonoBehaviour
+{
+    CharacterController charCtrl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        charCtrl = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hit;
+        Vector3 p1 = transform.position + charCtrl.center;
+        float distanceToObstacle = 0;
+        
+        if (Physics.SphereCast(p1, charCtrl.height / 2, transform.forward, out hit, 10))
+        {
+            distanceToObstacle = hit.distance;
+        }
+
+    }
+}
