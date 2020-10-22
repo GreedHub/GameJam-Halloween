0461c9a61789c5f2aaca3dec6b027a314d2c3f42
Lucas Julián Güerci <lucas.guerci@gmail.com>
2020-10-21 21:25:23 -0300

    Added basic player controller

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] Transform playerTransform;
+
+    float verticalRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseRotation = GetMouseRotation();
+
+        verticalRotation -= mouseRotation.y; 
+        verticalRotation = Mathf.Clamp(verticalRotation,-90f,90f);
+
+        transform.localRotation = Quaternion.Euler(verticalRotation,0f,0f);
+        playerTransform.Rotate(Vector3.up * mouseRotation.x);
+    }
+
+
+    Vector2 GetMouseRotation(){
+
+        Vector2 mouseRot = Vector2.zero;
+        mouseRot.x = Input.GetAxis("Mouse X");
+        mouseRot.y = Input.GetAxis("Mouse Y");
+
+        return mouseRot * mouseSensitivity * Time.deltaTime;
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private CharacterController controller;
+    [SerializeField] private float speed = 10f;
+    private Vector3 velocity;
+    private float gravity = -9.81f;
+    [SerializeField] Transform groundCheck;
+    float groundDistance = .4f;
+    [SerializeField] LayerMask groundMask;
+
+    void Start(){
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        Debug.Log(IsGrounded());
+        Debug.Log("vy1: "+velocity.y);
+
+        if(IsGrounded() && velocity.y < 0) velocity.y = -.2f;
+        
+        
+        Vector3 rawDirection = GetInput();
+
+        Vector3 move = transform.right * rawDirection.x + transform.forward * rawDirection.z;
+        
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime * Time.deltaTime;
+        Debug.Log("vy2: "+velocity.y);
+
+        controller.Move(velocity);
+    }
+
+    Vector3 GetInput(){
+
+        Vector3 _input = Vector3.zero;
+        _input.x = Input.GetAxis("Horizontal");
+        _input.z = Input.GetAxis("Vertical");
+       
+        return _input;
+    }
+
+    bool IsGrounded(){
+        return Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+    }
+}
